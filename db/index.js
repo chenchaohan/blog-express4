91cab2eb09b4f86e4b8de08111ccdca7b4805855
何武骏 <hewujun@supconit.com>
2024-01-14 18:15:03 +0800

    fix:修改服务器为阿里云

--- db/index.js
+++ db/index.js
@@ -2,10 +2,15 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
     //连接远程数据库
-    host: '101.33.249.237',
+    host: '114.55.75.3',
     user: 'root',
-    password: 'hewujun1027',
+    password: '123456',
     database: 'blog'
+    //连接本地数据库
+    // host: '127.0.0.1',
+    // user: 'root',
+    // password: 'hewujun1027',
+    // database: 'blogsql'
 })
 
 connection.connect()
