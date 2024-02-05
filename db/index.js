a0f6f020aa75af9d600d4d79c7582cae4607f18b
何武骏 <hewujun@supconit.com>
2024-02-05 09:35:34 +0800

    fix:修改数据库报错问题

--- db/index.js
+++ db/index.js
@@ -7,7 +7,11 @@ var connection = mysql.createConnection({
     password: 'hewujun1027',
     database: 'blog',
     port: 3307, // 端口号，默认3306
-    useConnectionPooling: true  // 解决连接数据库报错问题
+    useConnectionPooling: true,  // 解决连接数据库报错问题
+    debug: true,
+    // 解决错误Error: Cannot enqueue Query after fatal error.
+    supportBigNumbers: true,
+    bigNumberStrings: true
     //连接本地数据库
     // host: '127.0.0.1',
     // user: 'root',
@@ -15,6 +19,20 @@ var connection = mysql.createConnection({
     // database: 'blogsql'
 })
 
-connection.connect()
+connection.connect((err) => {
+  if (err) {
+    console.error('连接错误: ' + err.stack)
+    return
+  }
+  console.log('数据库连接进程id ' + connection.threadId)
+  // connection.query('SELECT * FROM users', (error, results, fields) => {
+  //   if (error) {
+  //     console.log('测试查询错误:', error)
+  //     return
+  //   }
+
+  //   console.log('查询用户测试:', results)
+  // })
+})
 
 module.exports = connection
